d44471a012fca36802c75b2386410a4f182fea22
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-07-23 21:16:31 +0530

    feat: initialize express server and socket.io connection

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+
+io.on("connection", (client) => {
+  console.log("A user connected:", client.id);
+  client.on("event", (data) => {});
+  client.on("disconnect", () => {
+    console.log("A user disconnected", client.id);
+  });
+});
+server.listen(5000, () => {
+  console.log("server running in port 5000");
+});
